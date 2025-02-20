0222201ad9701fc20834fef7b1d294507415619a
AADH4 <118476960+AADH4@users.noreply.github.com>
2025-02-20 17:38:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import time
 from streamlit_custom_notification_box import custom_notification_box
 
 # Initialize session state variables
+if 'selected_topic' not in st.session_state:
+    st.session_state.selected_topic = ""  # Or some other appropriate default value
+if 'start_time' not in st.session_state:
+    st.session_state.start_time = 0
 if 'last_update' not in st.session_state:
     st.session_state.last_update = 0
 if 'warnings_shown' not in st.session_state:
     st.session_state.warnings_shown = set()
 
+
 def timer_screen():
-    st.write(f"<h1 style='color: #008CBA; text-align: center;'>{st.session_state.selected_topic.upper()}</h1>", unsafe_allow_html=True)
+    if 'selected_topic' in st.session_state:
+        st.write(f"<h1 style='color: #008CBA; text-align: center;'>{st.session_state.selected_topic.upper()}</h1>", unsafe_allow_html=True)
+    else:
+        st.write("<h1 style='color: #008CBA; text-align: center;'>No Topic Selected</h1>", unsafe_allow_html=True)
     
     current_time = time.time()
     elapsed_time = int(current_time - st.session_state.start_time)
@@ -58,7 +66,7 @@ def timer_screen():
             st.session_state.balloons_shown = True
     
     if st.button("Reset and Start Over"):
-        for key in ['form_submitted', 'topic_selected', 'warnings_shown', 'balloons_shown', 'last_update']:
+        for key in ['form_submitted', 'topic_selected', 'warnings_shown', 'balloons_shown', 'last_update', 'selected_topic','start_time']:
             if key in st.session_state:
                 del st.session_state[key]
         st.session_state.screen = "topics"
@@ -80,4 +88,3 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 timer_screen()
-
